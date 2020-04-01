77d7c286fcb7b1eb15a170171f02ce7d0363a36d
jgiebelhausen <jgiebelhausen@seibert-media.net>
2020-04-01 15:58:37 +0200

    Write info log if user could not be found by api key

--- frappe/api.py
+++ frappe/api.py
@@ -183,7 +183,10 @@ def validate_api_key_secret(api_key, api_secret):
 		fieldname=['name']
 	)
 	form_dict = frappe.local.form_dict
-	user_secret = frappe.utils.password.get_decrypted_password ("User", user, fieldname='api_secret')
-	if api_secret == user_secret:
-		frappe.set_user(user)
-		frappe.local.form_dict = form_dict
+	if not user:
+		frappe.logger().info(f"no user found for api key '{api_key}'")
+	else:
+		user_secret = frappe.utils.password.get_decrypted_password("User", user, fieldname='api_secret')
+		if api_secret == user_secret:
+			frappe.set_user(user)
+			frappe.local.form_dict = form_dict
